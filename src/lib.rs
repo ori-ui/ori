73937f4a0e6186a17c037a4ad726040f0672ab66
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2023-05-30 15:55:08 +0200

    update lib example

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@
 //!     let counter = cx.signal(0);
 //!
 //!     view! {
-//!         <Button on:press=move |_| *counter.modify() += 1>
+//!         <Button on:click=move |_| *counter.modify() += 1>
 //!             "Click me!"
 //!         </Button>
 //!         { format!("Clicked {} times", counter.get()) }

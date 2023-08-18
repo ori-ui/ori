2a0047d1c964b013d44cdf3df4411c777f043f76
anon <Hjalte Nannestad>
2023-08-18 22:47:55 +0200

    Set event loop to Wait

--- crates/ori-wgpu/src/run.rs
+++ crates/ori-wgpu/src/run.rs
@@ -63,89 +63,93 @@ pub(crate) fn run<T: 'static>(mut app: App<T>) -> Result<(), Error> {
 
     app.ui.add_window(app.builder, window, render);
 
-    event_loop.run(move |event, _, control_flow| match event {
-        Event::RedrawEventsCleared => {
-            app.ui.idle();
-        }
-        Event::RedrawRequested(window_id) => {
-            let id = ids[&window_id];
-            app.ui.render(id);
-        }
-        Event::WindowEvent { window_id, event } => {
-            let window_id = ids[&window_id];
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
 
-            match event {
-                WindowEvent::CloseRequested => {
-                    *control_flow = ControlFlow::Exit;
-                }
-                WindowEvent::Resized(_) | WindowEvent::ScaleFactorChanged { .. } => {
-                    app.ui.resized(window_id);
-                }
-                WindowEvent::CursorMoved {
-                    device_id,
-                    position,
-                    ..
-                } => {
-                    app.ui.pointer_moved(
-                        window_id,
-                        PointerId::from_hash(&device_id),
-                        Vec2::new(position.x as f32, position.y as f32),
-                    );
-                }
-                WindowEvent::CursorLeft { device_id } => {
-                    (app.ui).pointer_left(window_id, PointerId::from_hash(&device_id));
-                }
-                WindowEvent::MouseInput {
-                    device_id,
-                    state,
-                    button,
-                    ..
-                } => {
-                    app.ui.pointer_button(
-                        window_id,
-                        PointerId::from_hash(&device_id),
-                        convert_mouse_button(button),
-                        is_pressed(state),
-                    );
-                }
-                WindowEvent::MouseWheel {
-                    delta: MouseScrollDelta::LineDelta(x, y),
-                    device_id,
-                    ..
-                } => {
-                    app.ui.pointer_scroll(
-                        window_id,
-                        PointerId::from_hash(&device_id),
-                        Vec2::new(x, y),
-                    );
-                }
-                WindowEvent::KeyboardInput {
-                    input:
-                        KeyboardInput {
-                            virtual_keycode: Some(keycode),
-                            state,
-                            ..
-                        },
-                    ..
-                } => {
-                    if let Some(key) = convert_key(keycode) {
-                        app.ui.keyboard_key(window_id, key, is_pressed(state));
-                    }
-                }
-                WindowEvent::ReceivedCharacter(c) => {
-                    app.ui.keyboard_char(window_id, c);
-                }
-                WindowEvent::ModifiersChanged(modifiers) => {
-                    app.ui.modifiers_changed(Modifiers {
-                        shift: modifiers.shift(),
-                        ctrl: modifiers.ctrl(),
-                        alt: modifiers.alt(),
-                        meta: modifiers.logo(),
-                    });
-                }
-                _ => {}
+        match event {
+            Event::RedrawEventsCleared => {
+                app.ui.idle();
             }
+            Event::RedrawRequested(window_id) => {
+                let id = ids[&window_id];
+                app.ui.render(id);
+            }
+            Event::WindowEvent { window_id, event } => {
+                let window_id = ids[&window_id];
+
+                match event {
+                    WindowEvent::CloseRequested => {
+                        *control_flow = ControlFlow::Exit;
+                    }
+                    WindowEvent::Resized(_) | WindowEvent::ScaleFactorChanged { .. } => {
+                        app.ui.resized(window_id);
+                    }
+                    WindowEvent::CursorMoved {
+                        device_id,
+                        position,
+                        ..
+                    } => {
+                        app.ui.pointer_moved(
+                            window_id,
+                            PointerId::from_hash(&device_id),
+                            Vec2::new(position.x as f32, position.y as f32),
+                        );
+                    }
+                    WindowEvent::CursorLeft { device_id } => {
+                        (app.ui).pointer_left(window_id, PointerId::from_hash(&device_id));
+                    }
+                    WindowEvent::MouseInput {
+                        device_id,
+                        state,
+                        button,
+                        ..
+                    } => {
+                        app.ui.pointer_button(
+                            window_id,
+                            PointerId::from_hash(&device_id),
+                            convert_mouse_button(button),
+                            is_pressed(state),
+                        );
+                    }
+                    WindowEvent::MouseWheel {
+                        delta: MouseScrollDelta::LineDelta(x, y),
+                        device_id,
+                        ..
+                    } => {
+                        app.ui.pointer_scroll(
+                            window_id,
+                            PointerId::from_hash(&device_id),
+                            Vec2::new(x, y),
+                        );
+                    }
+                    WindowEvent::KeyboardInput {
+                        input:
+                            KeyboardInput {
+                                virtual_keycode: Some(keycode),
+                                state,
+                                ..
+                            },
+                        ..
+                    } => {
+                        if let Some(key) = convert_key(keycode) {
+                            app.ui.keyboard_key(window_id, key, is_pressed(state));
+                        }
+                    }
+                    WindowEvent::ReceivedCharacter(c) => {
+                        app.ui.keyboard_char(window_id, c);
+                    }
+                    WindowEvent::ModifiersChanged(modifiers) => {
+                        app.ui.modifiers_changed(Modifiers {
+                            shift: modifiers.shift(),
+                            ctrl: modifiers.ctrl(),
+                            alt: modifiers.alt(),
+                            meta: modifiers.logo(),
+                        });
+                    }
+                    _ => {}
+                }
+            }
+            _ => {}
         }
-        _ => {}
     });
 }

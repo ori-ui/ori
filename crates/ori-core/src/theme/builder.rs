a0cd9c917617f06acc45bb0d0c060e79913a5dac
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2023-09-26 13:37:26 +0200

    Improve ThemeBuilder

--- crates/ori-core/src/theme/builder.rs
+++ crates/ori-core/src/theme/builder.rs
@@ -19,9 +19,24 @@ impl ThemeBuilder {
         Self::default()
     }
 
+    /// Get the number of theme builders.
+    pub fn len(&self) -> usize {
+        self.builders.len()
+    }
+
+    /// Check if the theme builders are empty.
+    pub fn is_empty(&self) -> bool {
+        self.builders.is_empty()
+    }
+
     /// Push a theme builder.
     pub fn push(&mut self, builder: impl FnMut() -> Theme + 'static) {
         self.builders.push(Box::new(builder));
+    }
+
+    /// Clear the theme builders.
+    pub fn clear(&mut self) {
+        self.builders.clear();
     }
 
     /// Build the theme.
@@ -35,6 +50,8 @@ impl ThemeBuilder {
 
 impl Debug for ThemeBuilder {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("ThemeBuilder").finish()
+        f.debug_struct("ThemeBuilder")
+            .field("builders", &self.builders.len())
+            .finish()
     }
 }

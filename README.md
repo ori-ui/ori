83b72153290fac2e4bc15aff6d9838b43517324d
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2023-09-27 22:05:31 +0200

    Add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ori
+Ori is a declarative UI framework focusing on simplicity and performance.
+
+# Examples
+Examples can be found at [examples](#examples/)
--- examples/counter.rs
+++ examples/counter.rs
@@ -1,29 +1,50 @@
 use ori::prelude::*;
 
+// We create our application data struct that holds all the state of our app.
+//
+// In this case, we only need a counter.
 #[derive(Default)]
 struct Data {
     counter: u32,
 }
 
+// We create a counter button component that increments the `Data::counter` when clicked.
+//
+// This returns a type that implements the `View` trait with `Data`.
 fn counter_button() -> impl View<Data> {
-    let button = button(text("Click me!"))
-        .fancy(4.0)
-        .on_click(|_, data: &mut Data| data.counter += 1);
+    // We create a button with the text "Click me!", and a fancy value of `pt(4.0)`.
+    //
+    // `pt` uses the scale fractor from the window to convert points to pixels.
+    let counter = button(text("Click me!")).fancy(pt(4.0));
 
-    alt("Counter Button", button)
+    // We use the `on_click` function to attach a callback to the button that
+    // increments the counter.
+    //
+    // Note that the callback is a closure that takes a mutable reference to
+    // an `EventCx` and a mutable reference to the `Data` struct.
+    let counter = on_click(counter, |_, data: &mut Data| data.counter += 1);
+
+    alt("Counter Button", counter)
 }
 
+// We create our app function that creates the UI of our app.
+//
+// This will be called every time the UI needs to be rebuilt,
+// eg. when the a button is clicked.
 fn app(data: &mut Data) -> impl View<Data> {
-    center(vstack![
-        rotate(data.counter as f32 * 0.3, counter_button()),
-        alt(
-            "Number of times the button has been clicked",
-            text(format!("Clicked {} time(s)", data.counter))
-        )
-    ])
+    // We use the `vstack!` macro to create a vertical stack of views.
+    let content = vstack![
+        counter_button(),
+        text(format!("Clicked {} time(s)", data.counter))
+    ];
+
+    // We use the `center` function to center the content in the window.
+    center(content)
 }
 
 fn main() {
+    // We create a new app with our `app` function and initial `Data` struct.
+    // Then we set the title of the window and run the app.
     App::new(app, Data::default())
         .title("Counter (examples/counter.rs)")
         .run();

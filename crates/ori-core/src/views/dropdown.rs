1339df0de2160b24f0ce1d77a5a3cbe8fbf99428
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2023-11-18 04:42:16 +0100

    Add Dropdown view

---
+++ crates/ori-core/src/views/dropdown.rs
@@ -0,0 +1,121 @@
+use crate::{
+    canvas::Canvas,
+    event::{Event, PointerEvent},
+    layout::{Size, Space, Vector},
+    view::{BuildCx, DrawCx, EventCx, LayoutCx, Pod, RebuildCx, State, View},
+};
+
+/// Create a new [`Dropdown`] view.
+pub fn dropdown<H, V>(header: H, content: V) -> Dropdown<H, V> {
+    Dropdown::new(header, content)
+}
+
+/// A dropdown view.
+pub struct Dropdown<H, V> {
+    /// The header of the dropdown.
+    pub header: H,
+    /// The content of the dropdown.
+    pub content: Pod<V>,
+}
+
+impl<H, V> Dropdown<H, V> {
+    /// Create a new dropdown.
+    pub fn new(header: H, content: V) -> Self {
+        Self {
+            header,
+            content: Pod::new(content),
+        }
+    }
+}
+
+impl<T, H: View<T>, V: View<T>> View<T> for Dropdown<H, V> {
+    type State = (H::State, State<T, V>);
+
+    fn build(&mut self, cx: &mut BuildCx, data: &mut T) -> Self::State {
+        (self.header.build(cx, data), self.content.build(cx, data))
+    }
+
+    fn rebuild(
+        &mut self,
+        (header, content): &mut Self::State,
+        cx: &mut RebuildCx,
+        data: &mut T,
+        old: &Self,
+    ) {
+        self.header.rebuild(header, cx, data, &old.header);
+        self.content.rebuild(content, cx, data, &old.content);
+    }
+
+    fn event(
+        &mut self,
+        (header, content): &mut Self::State,
+        cx: &mut EventCx,
+        data: &mut T,
+        event: &Event,
+    ) {
+        self.header.event(header, cx, data, event);
+        self.content.event(content, cx, data, event);
+
+        if event.is_handled() {
+            return;
+        }
+
+        if let Some(pointer) = event.get::<PointerEvent>() {
+            if pointer.is_press() && (cx.is_hot() || cx.is_focused()) && !content.has_hot() {
+                cx.set_focused(!cx.is_focused());
+                cx.request_draw();
+            }
+        }
+    }
+
+    fn layout(
+        &mut self,
+        (header, content): &mut Self::State,
+        cx: &mut LayoutCx,
+        data: &mut T,
+        space: Space,
+    ) -> Size {
+        let mut header_size = self.header.layout(header, cx, data, space);
+
+        // make sure the content is at least as wide as the header
+        let content_space = Space {
+            min: Size::new(header_size.width, 0.0),
+            max: cx.window().size(),
+        };
+        let content_size = self.content.layout(content, cx, data, content_space);
+
+        // if the content is wider than the header, make the header as wide as the content
+        if content_size.width > header_size.width {
+            let header_space = Space {
+                min: Size::new(content_size.width, space.min.height),
+                max: Size::new(content_size.width, space.max.height),
+            };
+
+            header_size = self.header.layout(header, cx, data, header_space);
+        }
+
+        // translate the content below the header
+        content.translate(Vector::new(0.0, header_size.height));
+
+        header_size
+    }
+
+    fn draw(
+        &mut self,
+        (header, content): &mut Self::State,
+        cx: &mut DrawCx,
+        data: &mut T,
+        canvas: &mut Canvas,
+    ) {
+        canvas.view(cx.id());
+        self.header.draw(header, cx, data, canvas);
+
+        if !cx.is_focused() {
+            return;
+        }
+
+        let mut layer = canvas.layer();
+        layer.depth += 10000.0;
+        self.content.draw(content, cx, data, &mut layer);
+    }
+}
---
+++ examples/dropdown.rs
@@ -0,0 +1,21 @@
+use ori::prelude::*;
+
+struct Data;
+
+fn app(_data: &mut Data) -> impl View<Data> {
+    let header = button(text("header"));
+    let content = container(pad(pt(8.0), text("content")))
+        .border_width(pt(2.0))
+        .border_color(style(Palette::SECONDARY_DARKER))
+        .border_radius(pt(6.0));
+
+    let content = dropdown(header, content);
+
+    center(content)
+}
+
+fn main() {
+    Launcher::new(app, Data)
+        .title("Dropdown (examples/dropdown.rs)")
+        .launch();
+}

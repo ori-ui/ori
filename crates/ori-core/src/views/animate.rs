1212746f4d094fda9dfa2d4420c41bd1df06a4fa
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2023-11-25 00:42:29 +0100

    Only update transitions when necessary

--- crates/ori-core/src/views/animate.rs
+++ crates/ori-core/src/views/animate.rs
@@ -9,7 +9,7 @@ use crate::{
 
 /// Create a new [`Animate`].
 pub fn animate<T, V, S>(
-    animate: impl FnMut(&mut S, &mut EventCx, &mut T, &Event) -> V + 'static,
+    animate: impl FnMut(&mut S, &mut EventCx, &mut T, &Event) -> Option<V> + 'static,
 ) -> Animate<T, V, S> {
     Animate::new(animate)
 }
@@ -19,6 +19,8 @@ pub fn transition_hot<T, V>(
     transition: Transition,
     mut view: impl FnMut(&mut EventCx, f32) -> V + 'static,
 ) -> Animate<T, V, f32> {
+    let mut built = false;
+
     animate(move |t: &mut f32, cx, _data: &mut T, event| {
         if event.is::<HotChanged>() {
             cx.request_animation_frame();
@@ -27,10 +29,16 @@ pub fn transition_hot<T, V>(
         if let Some(AnimationFrame(dt)) = event.get() {
             if transition.step(t, cx.is_hot(), *dt) {
                 cx.request_animation_frame();
+                return Some(view(cx, transition.get(*t)));
             }
         }
 
-        view(cx, transition.get(*t))
+        if !built {
+            built = true;
+            Some(view(cx, transition.get(*t)))
+        } else {
+            None
+        }
     })
 }
 
@@ -39,6 +47,8 @@ pub fn transition_active<T, V>(
     transition: Transition,
     mut view: impl FnMut(&mut EventCx, f32) -> V + 'static,
 ) -> Animate<T, V, f32> {
+    let mut built = false;
+
     animate(move |t: &mut f32, cx, _data: &mut T, event| {
         if event.is::<ActiveChanged>() {
             cx.request_animation_frame();
@@ -47,10 +57,46 @@ pub fn transition_active<T, V>(
         if let Some(AnimationFrame(dt)) = event.get() {
             if transition.step(t, cx.is_active(), *dt) {
                 cx.request_animation_frame();
+                return Some(view(cx, transition.get(*t)));
             }
         }
 
-        view(cx, transition.get(*t))
+        if !built {
+            built = true;
+            Some(view(cx, transition.get(*t)))
+        } else {
+            None
+        }
+    })
+}
+
+/// Animate a view when focused changes.
+pub fn transition_focused<T, V>(
+    transition: Transition,
+    mut view: impl FnMut(&mut EventCx, f32) -> V + 'static,
+) -> Animate<T, V, f32> {
+    let mut built = false;
+    let mut was_focused = false;
+
+    animate(move |t: &mut f32, cx, _data: &mut T, event| {
+        if cx.is_focused() != was_focused {
+            was_focused = cx.is_focused();
+            cx.request_animation_frame();
+        }
+
+        if let Some(AnimationFrame(dt)) = event.get() {
+            if transition.step(t, cx.is_focused(), *dt) {
+                cx.request_animation_frame();
+                return Some(view(cx, transition.get(*t)));
+            }
+        }
+
+        if !built {
+            built = true;
+            Some(view(cx, transition.get(*t)))
+        } else {
+            None
+        }
     })
 }
 
@@ -60,14 +106,22 @@ pub fn transition<T, V>(
     active: bool,
     mut view: impl FnMut(&mut EventCx, f32) -> V + 'static,
 ) -> Animate<T, V, f32> {
+    let mut built = false;
+
     animate(move |t: &mut f32, cx, _data: &mut T, event| {
         if let Some(AnimationFrame(dt)) = event.get() {
             if transition.step(t, active, *dt) {
                 cx.request_animation_frame();
+                return Some(view(cx, transition.get(*t)));
             }
         }
 
-        view(cx, transition.get(*t))
+        if !built {
+            built = true;
+            Some(view(cx, transition.get(*t)))
+        } else {
+            None
+        }
     })
 }
 
@@ -77,14 +131,16 @@ pub fn transition<T, V>(
 pub struct Animate<T, V, S = ()> {
     /// The animation callback.
     #[allow(clippy::type_complexity)]
-    pub animate: Box<dyn FnMut(&mut S, &mut EventCx, &mut T, &Event) -> V>,
+    pub animate: Box<dyn FnMut(&mut S, &mut EventCx, &mut T, &Event) -> Option<V>>,
     /// The theme to apply when building the view.
     pub theme: Theme,
 }
 
 impl<T, V, S> Animate<T, V, S> {
     /// Create a new [`Animate`].
-    pub fn new(animate: impl FnMut(&mut S, &mut EventCx, &mut T, &Event) -> V + 'static) -> Self {
+    pub fn new(
+        animate: impl FnMut(&mut S, &mut EventCx, &mut T, &Event) -> Option<V> + 'static,
+    ) -> Self {
         Self {
             animate: Box::new(animate),
             theme: theme_snapshot(),
@@ -121,21 +177,38 @@ impl<T, V: View<T>, S: Default> View<T> for Animate<T, V, S> {
     }
 
     fn event(&mut self, state: &mut Self::State, cx: &mut EventCx, data: &mut T, event: &Event) {
-        let mut new_view = Theme::with_global(&mut self.theme, || {
+        fn update_view<T, V: View<T>>(
+            cx: &mut EventCx,
+            data: &mut T,
+            state: &mut Option<(V::State, V)>,
+            mut new_view: V,
+            event: &Event,
+        ) {
+            if let Some((ref mut view_state, ref mut old_view)) = state {
+                new_view.rebuild(view_state, &mut cx.rebuild_cx(), data, old_view);
+                new_view.event(view_state, cx, data, event);
+
+                *old_view = new_view;
+            } else {
+                let mut new_state = new_view.build(&mut cx.build_cx(), data);
+
+                new_view.event(&mut new_state, cx, data, event);
+                *state = Some((new_state, new_view));
+            }
+        }
+
+        let new_view = Theme::with_global(&mut self.theme, || {
             (self.animate)(&mut state.animate_state, cx, data, event)
         });
 
-        if let Some((ref mut view_state, ref mut old_view)) = state.view {
-            new_view.rebuild(view_state, &mut cx.rebuild_cx(), data, old_view);
-            new_view.event(view_state, cx, data, event);
-
-            *old_view = new_view;
-        } else {
-            let mut new_state = new_view.build(&mut cx.build_cx(), data);
-
-            new_view.event(&mut new_state, cx, data, event);
-            state.view = Some((new_state, new_view));
-        }
+        match new_view {
+            Some(new_view) => update_view(cx, data, &mut state.view, new_view, event),
+            None => {
+                if let Some((ref mut view_state, ref mut view)) = state.view {
+                    view.event(view_state, cx, data, event);
+                }
+            }
+        };
     }
 
     fn layout(

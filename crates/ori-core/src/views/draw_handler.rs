a4ea4c96b2b653747a780967a78db2fe7921f8d4
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2024-03-11 04:06:23 +0100

    Add on_draw and DrawHandler

---
+++ crates/ori-core/src/views/draw_handler.rs
@@ -0,0 +1,82 @@
+use crate::{
+    canvas::Canvas,
+    event::Event,
+    layout::{Size, Space},
+    view::{BuildCx, DrawCx, EventCx, LayoutCx, RebuildCx, View},
+};
+
+/// Create a new [`DrawHandler`].
+pub fn on_draw<T, V>(
+    content: V,
+    on_draw: impl FnMut(&mut DrawCx, &mut T, &mut Canvas) + 'static,
+) -> DrawHandler<T, V> {
+    DrawHandler::new(content).on_draw(on_draw)
+}
+
+/// A view that hooks into the draw cycle.
+pub struct DrawHandler<T, V> {
+    /// The content.
+    pub content: V,
+    /// The draw callback.
+    #[allow(clippy::type_complexity)]
+    pub on_draw: Option<Box<dyn FnMut(&mut DrawCx, &mut T, &mut Canvas) + 'static>>,
+}
+
+impl<T, V> DrawHandler<T, V> {
+    /// Create a new [`DrawHandler`].
+    pub fn new(content: V) -> Self {
+        Self {
+            content,
+            on_draw: Option::None,
+        }
+    }
+
+    /// Set the draw callback.
+    pub fn on_draw(
+        mut self,
+        on_draw: impl FnMut(&mut DrawCx, &mut T, &mut Canvas) + 'static,
+    ) -> Self {
+        self.on_draw = Some(Box::new(on_draw));
+        self
+    }
+}
+
+impl<T, V: View<T>> View<T> for DrawHandler<T, V> {
+    type State = V::State;
+
+    fn build(&mut self, cx: &mut BuildCx, data: &mut T) -> Self::State {
+        self.content.build(cx, data)
+    }
+
+    fn rebuild(&mut self, state: &mut Self::State, cx: &mut RebuildCx, data: &mut T, old: &Self) {
+        self.content.rebuild(state, cx, data, &old.content);
+    }
+
+    fn event(&mut self, state: &mut Self::State, cx: &mut EventCx, data: &mut T, event: &Event) {
+        self.content.event(state, cx, data, event);
+    }
+
+    fn layout(
+        &mut self,
+        state: &mut Self::State,
+        cx: &mut LayoutCx,
+        data: &mut T,
+        space: Space,
+    ) -> Size {
+        self.content.layout(state, cx, data, space)
+    }
+
+    fn draw(
+        &mut self,
+        state: &mut Self::State,
+        cx: &mut DrawCx,
+        data: &mut T,
+        canvas: &mut Canvas,
+    ) {
+        self.content.draw(state, cx, data, canvas);
+
+        if let Some(ref mut on_draw) = self.on_draw {
+            on_draw(cx, data, canvas);
+        }
+    }
+}

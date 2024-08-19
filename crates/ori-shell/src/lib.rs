4750eb98103496a9c465d8cb52119bf136a9a4d6
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2024-08-19 03:35:49 +0200

    Initialize logging in ori_shell::launch

--- crates/ori-shell/src/lib.rs
+++ crates/ori-shell/src/lib.rs
@@ -5,6 +5,7 @@
 
 use ori_app::{AppBuilder, IntoUiBuilder};
 use ori_core::window::Window;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter};
 
 pub mod platform;
 
@@ -44,6 +45,30 @@ impl std::error::Error for Error {}
 /// Launch an Ori application.
 #[allow(unused_variables)]
 pub fn launch<T>(app: AppBuilder<T>, data: T) -> Result<(), Error> {
+    let mut filter = EnvFilter::default().add_directive(tracing::Level::DEBUG.into());
+
+    if let Ok(env) = std::env::var("RUST_LOG") {
+        filter = filter.add_directive(env.parse().unwrap());
+    }
+
+    let subscriber = tracing_subscriber::registry().with(filter);
+
+    #[cfg(not(target_arch = "wasm32"))]
+    let subscriber = {
+        let fmt_layer = tracing_subscriber::fmt::Layer::default();
+        subscriber.with(fmt_layer)
+    };
+
+    #[cfg(target_arch = "wasm32")]
+    let subscriber = {
+        let wasm_layer = tracing_wasm::WASMLayer::new(Default::default());
+        subscriber.with(fmt_layer)
+    };
+
+    if let Err(err) = tracing::subscriber::set_global_default(subscriber) {
+        eprintln!("Failed to set global default subscriber: {}", err);
+    }
+
     #[cfg(x11_platform)]
     {
         let app = platform::x11::X11App::new(app, data)?;

9b85799dc7ddc0e8045e51de14a78e1a43998541
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2024-08-23 16:01:55 +0200

    Change interface for clickable

--- crates/ori-core/src/views/clickable.rs
+++ crates/ori-core/src/views/clickable.rs
@@ -1,3 +1,5 @@
+use std::marker::PhantomData;
+
 use ori_macro::Build;
 
 use crate::{
@@ -8,38 +10,53 @@ use crate::{
     view::{Pod, State, View},
 };
 
-/// Create a new [`Clickable`].
-pub fn clickable<T, V>(content: V) -> Clickable<T, V> {
-    Clickable::new(content)
+/// Create a new [`Clickable`], that calls `on_press` when pressed.
+pub fn on_press<T, V, F>(content: V, on_press: F) -> Clickable<T, V, F>
+where
+    V: View<T>,
+    F: FnMut(&mut EventCx, &mut T) + 'static,
+{
+    Clickable::new(content, ClickEvent::Press, on_press)
 }
 
-/// Create a new [`Clickable`], with an [`on_press`](Clickable::on_press()) callback.
-pub fn on_press<T, V>(
-    content: V,
-    on_press: impl FnMut(&mut EventCx, &mut T) + 'static,
-) -> Clickable<T, V> {
-    clickable(content).on_press(on_press)
+/// Create a new [`Clickable`], that calls `on_release` when released.
+pub fn on_release<T, V, F>(content: V, on_release: F) -> Clickable<T, V, F>
+where
+    V: View<T>,
+    F: FnMut(&mut EventCx, &mut T) + 'static,
+{
+    Clickable::new(content, ClickEvent::Release, on_release)
 }
 
-/// Create a new [`Clickable`], with an [`on_release`](Clickable::on_release()) callback.
-pub fn on_release<T, V>(
-    content: V,
-    on_release: impl FnMut(&mut EventCx, &mut T) + 'static,
-) -> Clickable<T, V> {
-    clickable(content).on_release(on_release)
+/// Create a new [`Clickable`], that calls `on_click` when clicked.
+pub fn on_click<T, V, F>(content: V, on_click: F) -> Clickable<T, V, F>
+where
+    V: View<T>,
+    F: FnMut(&mut EventCx, &mut T) + 'static,
+{
+    Clickable::new(content, ClickEvent::Click, on_click)
 }
 
-/// Create a new [`Clickable`], with an [`on_click`](Clickable::on_click()) callback.
-pub fn on_click<T, V>(
-    content: V,
-    on_click: impl FnMut(&mut EventCx, &mut T) + 'static,
-) -> Clickable<T, V> {
-    clickable(content).on_click(on_click)
+/// A click event.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub enum ClickEvent {
+    /// The press event.
+    Press,
+
+    /// The release event.
+    Release,
+
+    /// The click event.
+    Click,
 }
 
 /// A click handler.
 #[derive(Build, Rebuild)]
-pub struct Clickable<T, V> {
+pub struct Clickable<T, V, F>
+where
+    V: View<T>,
+    F: FnMut(&mut EventCx, &mut T) + 'static,
+{
     /// The content.
     pub content: Pod<V>,
 
@@ -53,51 +70,30 @@ pub struct Clickable<T, V> {
     /// If `Some` the callbacks will only be called when this button is pressed.
     pub button: Option<PointerButton>,
 
-    /// The callback for when the button is pressed.
-    #[build(ignore)]
-    #[allow(clippy::type_complexity)]
-    pub on_press: Option<Box<dyn FnMut(&mut EventCx, &mut T) + 'static>>,
+    /// The event to listen for.
+    pub event: ClickEvent,
 
-    /// The callback for when the button is released.
-    #[build(ignore)]
-    #[allow(clippy::type_complexity)]
-    pub on_release: Option<Box<dyn FnMut(&mut EventCx, &mut T) + 'static>>,
+    /// The callback.
+    pub callback: F,
 
-    /// The callback for when the button is clicked.
-    #[build(ignore)]
-    #[allow(clippy::type_complexity)]
-    pub on_click: Option<Box<dyn FnMut(&mut EventCx, &mut T) + 'static>>,
+    marker: PhantomData<fn() -> T>,
 }
 
-impl<T, V> Clickable<T, V> {
+impl<T, V, F> Clickable<T, V, F>
+where
+    V: View<T>,
+    F: FnMut(&mut EventCx, &mut T) + 'static,
+{
     /// Create a new [`Clickable`].
-    pub fn new(content: V) -> Self {
+    pub fn new(content: V, event: ClickEvent, callback: F) -> Self {
         Self {
             content: Pod::new(content),
             descendants: true,
             button: None,
-            on_press: None,
-            on_release: None,
-            on_click: None,
+            event,
+            callback,
+            marker: PhantomData,
         }
-    }
-
-    /// Set the callback for when the button is pressed.
-    pub fn on_press(mut self, on_press: impl FnMut(&mut EventCx, &mut T) + 'static) -> Self {
-        self.on_press = Some(Box::new(on_press));
-        self
-    }
-
-    /// Set the callback for when the button is released.
-    pub fn on_release(mut self, on_release: impl FnMut(&mut EventCx, &mut T) + 'static) -> Self {
-        self.on_release = Some(Box::new(on_release));
-        self
-    }
-
-    /// Set the callback for when the button is clicked.
-    pub fn on_click(mut self, on_click: impl FnMut(&mut EventCx, &mut T) + 'static) -> Self {
-        self.on_click = Some(Box::new(on_click));
-        self
     }
 
     fn is_button(&self, button: PointerButton) -> bool {
@@ -105,7 +101,11 @@ impl<T, V> Clickable<T, V> {
     }
 }
 
-impl<T, V: View<T>> View<T> for Clickable<T, V> {
+impl<T, V, F> View<T> for Clickable<T, V, F>
+where
+    V: View<T>,
+    F: FnMut(&mut EventCx, &mut T) + 'static,
+{
     type State = State<T, V>;
 
     fn build(&mut self, cx: &mut BuildCx, data: &mut T) -> Self::State {
@@ -123,24 +123,22 @@ impl<T, V: View<T>> View<T> for Clickable<T, V> {
 
         match event {
             Event::PointerPressed(e) if is_hot && self.is_button(e.button) => {
-                if let Some(ref mut on_press) = self.on_press {
-                    on_press(cx, data);
+                if self.event == ClickEvent::Press {
+                    (self.callback)(cx, data);
                     cx.request_rebuild();
                 }
 
                 content.set_active(true);
             }
             Event::PointerReleased(e) if content.is_active() && self.is_button(e.button) => {
-                if let Some(ref mut on_release) = self.on_release {
-                    on_release(cx, data);
+                if self.event == ClickEvent::Release {
+                    (self.callback)(cx, data);
                     cx.request_rebuild();
                 }
 
-                if e.clicked {
-                    if let Some(ref mut on_click) = self.on_click {
-                        on_click(cx, data);
-                        cx.request_rebuild();
-                    }
+                if e.clicked && self.event == ClickEvent::Click {
+                    (self.callback)(cx, data);
+                    cx.request_rebuild();
                 }
 
                 content.set_active(false);

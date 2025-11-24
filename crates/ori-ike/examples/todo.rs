d764c4275d6d6a36d6971fafa441faa6aad2831e
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2025-11-24 01:59:14 +0100

    expand todo example

--- crates/ori-ike/examples/todo.rs
+++ crates/ori-ike/examples/todo.rs
@@ -1,19 +1,67 @@
 use ori_ike::prelude::*;
 
 struct Data {
-    count: u32,
+    todos: Vec<Todo>,
 }
 
-fn counter(data: &mut Data) -> impl View<Data> + use<> {
-    center(entry().placeholder("What do you want to do?"))
+impl Data {
+    fn add_todo(&mut self, name: String) {
+        self.todos.push(Todo::new(name));
+    }
+}
+
+struct Todo {
+    name: String,
+}
+
+impl Todo {
+    fn new(name: String) -> Self {
+        Self { name }
+    }
+}
+
+fn name_entry() -> impl View<Data> + use<> {
+    entry()
+        .placeholder("What do you want to do?")
+        .on_submit(Data::add_todo)
+        .submit_behaviour(SubmitBehaviour {
+            keep_focus: true,
+            clear_text: true,
+        })
+}
+
+fn todo(i: usize, todo: &Todo) -> impl View<Data> + use<> {
+    container(
+        hstack((
+            button(label("x"), move |data: &mut Data| {
+                data.todos.remove(i);
+            }),
+            center(label(&todo.name)),
+        ))
+        .gap(12.0),
+    )
+}
+
+fn todos(data: &mut Data) -> impl View<Data> + use<> {
+    vstack(
+        data.todos
+            .iter()
+            .enumerate()
+            .rev()
+            .map(|(i, t)| todo(i, t))
+            .collect::<Vec<_>>(),
+    )
 }
 
 fn ui(data: &mut Data) -> impl Effect<Data> + use<> {
-    window(counter(data))
+    window(center(width(
+        200.0,
+        vstack((name_entry(), todos(data))),
+    )))
 }
 
 fn main() {
-    let mut data = Data { count: 0 };
+    let mut data = Data { todos: Vec::new() };
 
     App::new().run(&mut data, ui);
 }

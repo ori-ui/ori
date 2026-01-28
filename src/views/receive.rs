310abf68b381212ddc0b4440f38091a8035bdb5a
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2026-01-28 16:01:22 +0100

    fix: docs

--- src/views/receive.rs
+++ src/views/receive.rs
@@ -1,13 +1,13 @@
 use crate::{Action, Effect, Message, Mut, View, ViewMarker};
 
-/// [`View`] that handles message.
+/// [`View`] that receives message.
 pub fn receive_any<C, T>(
     on_message: impl FnMut(&mut T, &mut Message) -> Action,
 ) -> impl Effect<C, T> {
     Receive::new(on_message)
 }
 
-/// [`View`] that handles messages.
+/// [`View`] that receives messages.
 pub fn receive<C, T, E, A>(mut on_message: impl FnMut(&mut T, E) -> A) -> impl Effect<C, T>
 where
     E: Send + 'static,
@@ -22,7 +22,7 @@ where
     })
 }
 
-/// [`View`] that handles messages.
+/// [`View`] that receives messages.
 #[must_use]
 pub struct Receive<E> {
     on_message: E,

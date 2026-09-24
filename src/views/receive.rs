3c614105b8d6757a75c5065b0398cc3338b3d4ac
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2026-09-24 15:12:40 +0200

    fix: receive

--- src/views/receive.rs
+++ src/views/receive.rs
@@ -1,10 +1,14 @@
-use crate::{Action, Effect, Message, Mut, View, ViewId, ViewMarker};
+use crate::{Action, Effect, Message, Mut, Tracker, View, ViewId, ViewMarker};
 
 /// [`View`] that receives message.
 pub fn receive_all<C, T>(
+    view_id: impl Into<Option<ViewId>>,
     on_message: impl FnMut(&mut T, &mut Message) -> Action,
-) -> impl Effect<C, T> {
-    Receive::new(on_message)
+) -> impl Effect<C, T>
+where
+    C: Tracker,
+{
+    Receive::new(view_id.into(), on_message)
 }
 
 /// [`View`] that receives messages.
@@ -13,12 +17,13 @@ pub fn receive<C, T, E, A>(
     mut on_message: impl FnMut(&mut T, E) -> A,
 ) -> impl Effect<C, T>
 where
+    C: Tracker,
     E: Send + 'static,
     A: Into<Action>,
 {
     let view_id = view_id.into();
 
-    receive_all(move |data, event| {
+    receive_all(view_id, move |data, event| {
         let message = match view_id {
             Some(id) => event.take(id),
             None => event.take_untargeted(),
@@ -34,46 +39,70 @@ where
 /// [`View`] that receives messages.
 #[must_use]
 pub struct Receive<F> {
+    view_id:    Option<ViewId>,
     on_message: F,
 }
 
 impl<F> Receive<F> {
     /// Create new [`Receive`].
-    pub const fn new(on_message: F) -> Self {
-        Receive { on_message }
+    pub const fn new(view_id: Option<ViewId>, on_message: F) -> Self {
+        Receive {
+            view_id,
+            on_message,
+        }
     }
 }
 
 impl<F> ViewMarker for Receive<F> {}
 impl<C, T, F> View<C, T> for Receive<F>
 where
+    C: Tracker,
     F: FnMut(&mut T, &mut Message) -> Action,
 {
     type Element = ();
-    type State = F;
+    type State = Self;
 
-    fn build(self, _cx: &mut C, _data: &mut T) -> (Self::Element, Self::State) {
-        ((), self.on_message)
+    fn build(self, cx: &mut C, _data: &mut T) -> (Self::Element, Self::State) {
+        if let Some(id) = self.view_id {
+            cx.register(id);
+        }
+
+        ((), self)
     }
 
     fn rebuild(
         self,
         _element: Mut<'_, Self::Element>,
-        _state: &mut Self::State,
-        _cx: &mut C,
+        state: &mut Self::State,
+        cx: &mut C,
         _data: &mut T,
     ) {
+        if state.view_id != self.view_id {
+            if let Some(id) = state.view_id {
+                cx.unregister(id);
+            }
+
+            if let Some(id) = self.view_id {
+                cx.register(id);
+            }
+        }
+
+        *state = self;
     }
 
     fn message(
         _element: Mut<'_, Self::Element>,
-        on_message: &mut Self::State,
+        state: &mut Self::State,
         _cx: &mut C,
         data: &mut T,
         message: &mut Message,
     ) -> Action {
-        on_message(data, message)
+        (state.on_message)(data, message)
     }
 
-    fn teardown(_element: Self::Element, _state: Self::State, _cx: &mut C) {}
+    fn teardown(_element: Self::Element, state: Self::State, cx: &mut C) {
+        if let Some(id) = state.view_id {
+            cx.unregister(id);
+        }
+    }
 }
